2de8e8c54e100c1a85c34f78ae9dc61eb5ae1b59
kajal <aroskarkajal373@gmail.com>
2024-05-07 09:24:30 +0530

    modify index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -4,15 +4,16 @@ export const navLinks = [
     route: "/",
     icon: "/assets/icons/home.svg",
   },
-  {
-    label: "Image Restore",
-    route: "/transformations/add/restore",
-    icon: "/assets/icons/image.svg",
-  },
+  
   {
     label: "Generative Fill",
     route: "/transformations/add/fill",
     icon: "/assets/icons/stars.svg",
+  },
+  {
+    label: "Object Recolor",
+    route: "/transformations/add/recolor",
+    icon: "/assets/icons/filter.svg",
   },
   {
     label: "Object Remove",
@@ -20,10 +21,11 @@ export const navLinks = [
     icon: "/assets/icons/scan.svg",
   },
   {
-    label: "Object Recolor",
-    route: "/transformations/add/recolor",
-    icon: "/assets/icons/filter.svg",
+    label: "Image Restore",
+    route: "/transformations/add/restore",
+    icon: "/assets/icons/image.svg",
   },
+ 
   {
     label: "Background Remove",
     route: "/transformations/add/removeBackground",
